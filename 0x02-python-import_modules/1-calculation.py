723bc25f97c154059adc6a1b4c4dead72899f2ad
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-08-27 02:26:23 -0700

    calucaltion fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ import calculator_1
 a = 10
 b = 5
 
-print("% + % = %",a, b, calculator_1.add(a, b))
-print(calculator_1.sub(a, b)) 
-print(calculator_1.mul(a, b))
-print(calculator_1.div(a, b))
+print(a, ' + ', b, ' = ', calculator_1.add(a, b))
+print(a, ' - ', b, ' = ', calculator_1.sub(a, b)) 
+print(a, ' * ', b, ' = ', calculator_1.mul(a, b))
+print(a, ' / ', b, ' = ', calculator_1.div(a, b))
