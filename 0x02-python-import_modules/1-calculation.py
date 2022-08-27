f5492dc279403673a1535662c68063525dff525f
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-08-27 03:23:03 -0700

    import fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import calculator_1
+import calculator_1 as cal
 
 a = 10
 b = 5
 
-print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
