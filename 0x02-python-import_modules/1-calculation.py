f9184a8e8503d24684aea7cf39abb1c23f2d842e
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-08-27 03:05:46 -0700

    pscode fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,6 @@ a = 10
 b = 5
 
 print(a, ' + ', b, ' = ', calculator_1.add(a, b))
-print(a, ' - ', b, ' = ', calculator_1.sub(a, b)) 
+print(a, ' - ', b, ' = ', calculator_1.sub(a, b))
 print(a, ' * ', b, ' = ', calculator_1.mul(a, b))
 print(a, ' / ', b, ' = ', calculator_1.div(a, b))
